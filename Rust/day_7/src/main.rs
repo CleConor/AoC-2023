fc8b23751f1d7030d20684140d1630feff8395a6
CleConor <francesco98.cle@gmail.com>
2023-12-10 17:46:16 +0100

    day_7 template

--- Rust/day_7/src/main.rs
+++ Rust/day_7/src/main.rs
@@ -1,3 +1,45 @@
 fn main() {
-    println!("Hello, world!");
+    let input1 = include_str!("input1.txt");
+    let sol1 = first_part(input1);
+    dbg!(sol1);
+
+    /*let input2 = include_str!("input1.txt");
+    let sol2 = second_part(input2);
+    dbg!(sol2);*/
+
+}
+
+fn first_part(input: &str) -> usize {
+    let lines = input.split('\n');
+    
+    get_total_wining(lines)
+}
+
+/*fn second_part(input: &str) -> usize {
+    let lines = input.split('\n');
+    
+}*/
+
+
+fn get_total_wining<'a>(lines: impl Iterator<Item = &'a str>) -> usize {
+    8
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_first_part() {
+        let input = "";
+        let sol = first_part(input);
+        assert_eq!(sol, 35);
+    }
+
+    /*#[test]
+    fn test_second_part() {
+        let input = "";
+        let sol = second_part(input);
+        assert_eq!(sol, 46);
+    }*/
 }
